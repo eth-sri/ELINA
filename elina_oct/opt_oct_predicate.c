c59fc76d2f65b49e4f56a373460ca6efe46b2a02
gsingh <gsingh@student.ethz.ch>
2016-11-25 19:30:07 +0100

    polyhedra library added

--- elina_oct/opt_oct_predicate.c
+++ elina_oct/opt_oct_predicate.c
@@ -1,5 +1,5 @@
 /*
-	Copyright 2015 Software Reliability Lab, ETH Zurich
+	Copyright 2016 Software Reliability Lab, ETH Zurich
 
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
