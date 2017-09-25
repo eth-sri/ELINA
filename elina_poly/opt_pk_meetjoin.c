694cfe975a7dadd7d4aa51ed4b408afe2c29290b
GgnDpSngh <gsingh@student.ethz.ch>
2017-09-25 17:26:10 +0200

    optimized removal of common generators from cartesian product

--- elina_poly/opt_pk_meetjoin.c
+++ elina_poly/opt_pk_meetjoin.c
@@ -1324,6 +1324,7 @@ opt_pk_array_t * opt_poly_join_gen(elina_manager_t *man, opt_pk_array_t *oa, opt
           // combine all overlapping vertices of A into one
           count = cartesian_product_vertices_one_comp(
               poly1, acl, ca_arr, num_vertex1, poly[0], count, ca, pos_con_map);
+          remove_common_gen_upto(opk, poly[0]->F, 0, poly[0]->F->nbrows);
           // combine all overlapping rays of A into one
 
           meet_rays_one_comp(poly1, acl, ca_arr, nblinemapa, poly[0],
@@ -1348,7 +1349,7 @@ opt_pk_array_t * opt_poly_join_gen(elina_manager_t *man, opt_pk_array_t *oa, opt
                 free(ca);
                 opt_matrix_t * F = poly[0]->F;
 		opt_matrix_t * C = poly[0]->C;
-                remove_common_gen_upto(opk, F, 0, begin);
+
                 opt_matrix_sort_rows(opk,C);
 		/************************
 			Combine satmat of A
