ae3404f84c0ec4e746bdaa31eca70ca580029165
GgnDpSngh <gsingh@student.ethz.ch>
2017-10-04 16:09:55 +0200

    fixed exception handling in projection

--- elina_poly/opt_pk_project.c
+++ elina_poly/opt_pk_project.c
@@ -515,6 +515,7 @@ opt_pk_array_t* opt_pk_forget_array(elina_manager_t* man, bool destructive, opt_
 	/********************************
 		Handle the destructive case
 	*********************************/
+	//char * exc_map = (char *)calloc(num_compa,sizeof(char));
 	if(destructive){
 		k = 0;
 		while(k < num_compa){
@@ -668,6 +669,13 @@ opt_pk_array_t* opt_pk_forget_array(elina_manager_t* man, bool destructive, opt_
 				free(nca);
 				poly_a[k]->F = dst_mat;
 				opt_poly_chernikova(man,poly_a[k],"forget destructive");
+				if(opk->exn){
+					opk->exn = ELINA_EXC_NONE;
+					man->result.flag_best = man->result.flag_exact = false; 
+					opt_poly_set_top(opk,oa);
+					return oa; 
+						//exc_map[k] = 1;
+				}
 			}
 			cla = cla->next;
 			k++;
@@ -675,6 +683,27 @@ opt_pk_array_t* opt_pk_forget_array(elina_manager_t* man, bool destructive, opt_
 		#if defined(TIMING)
  	 		record_timing(forget_array_time);
    		#endif
+		/*unsigned short int k1=0;
+	  	unsigned short int bound = oa->acl->size;
+	  	cl = oa->acl->head;
+	  	for(k=0; k < num_comp;k++){
+	  		opt_pk_t *oak = oa->poly[k1];
+	  		if(exc_map[k]){
+	  			comp_list_t * tmp = cl;
+	  			cl = cl->next;
+	  			remove_comp_list(acl,tmp);
+	  			unsigned short int k2;
+	  			for(k2=k1; k2 < bound - 1; k2++){
+	  				poly[k2] = poly[k2+1];
+	  			}
+	  			opt_poly_clear(oak);
+	  			bound--;
+	  		}
+	  		else{
+	  			k1++;
+	  			cl=cl->next;
+	  		}
+	      	}*/
 		return oa;
 	}
 	/********************************
@@ -735,6 +764,7 @@ opt_pk_array_t* opt_pk_forget_array(elina_manager_t* man, bool destructive, opt_
 					   if(opk->funopt->algorithm>=0){
 					      opt_poly_chernikova(man,src,"of the result");
 					   }
+					  
 					}
 					else {
 	   				     /* Forget */
@@ -815,6 +845,11 @@ opt_pk_array_t* opt_pk_forget_array(elina_manager_t* man, bool destructive, opt_
 					free(nca);
 					poly[k1]->F = dst_mat;
 					opt_poly_chernikova(man,poly[k1],"forget non destructive");
+					if(opk->exn){
+						opk->exn = ELINA_EXC_NONE;
+						man->result.flag_best = man->result.flag_exact = false; 
+						return opt_pk_top(man,oa->maxcols-2,0);
+					}
 				}
 				else{
 					poly[k1] = opt_poly_alloc(src->intdim,src->realdim);
