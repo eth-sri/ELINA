f2b610da66bfa96a8d9ed83a60c368419c456dc6
GgnDpSngh <gsingh@inf.ethz.ch>
2018-12-16 22:55:25 +0100

    fixed memory leak

--- fppoly/fppoly.c
+++ fppoly/fppoly.c
@@ -813,6 +813,9 @@ double compute_lb_from_expr(fppoly_internal_t *pr, expr_t *expr, fppoly_t *fp) {
   }
   //	printf("inf: %g\n",-res_inf);
   //	fflush(stdout);
+  if (fp->input_lexpr != NULL && fp->input_uexpr != NULL) {
+    free_expr(expr);
+  }
   return res_inf;
 }
 
@@ -917,6 +920,9 @@ double compute_ub_from_expr(fppoly_internal_t *pr, expr_t *expr, fppoly_t *fp) {
   }
   // printf("sup: %g\n",res_sup);
   // fflush(stdout);
+  if (fp->input_lexpr != NULL && fp->input_uexpr != NULL) {
+    free_expr(expr);
+  }
   return res_sup;
 }
 
