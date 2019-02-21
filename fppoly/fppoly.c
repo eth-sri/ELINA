e7ef685eaba6bc43f789f3ff5c1578978d0776a2
Gagandeep Singh <gsingh@inf.ethz.ch>
2019-02-21 13:55:48 +0530

    allocation fix

--- fppoly/fppoly.c
+++ fppoly/fppoly.c
@@ -167,7 +167,7 @@ expr_t * create_sparse_expr(double *coeff, double cst, size_t *dim, size_t size)
 	if(size>0){
 		expr->inf_coeff = (double *)malloc(size*sizeof(double));
 		expr->sup_coeff = (double *)malloc(size*sizeof(double));
-		expr->dim = (size_t *)malloc(size*sizeof(double));
+		expr->dim = (size_t *)malloc(size*sizeof(size_t));
 	}
 	else{
 		expr->inf_coeff = NULL;
