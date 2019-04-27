248d10d94013db8f837e1f61af54a1f86c562441
GgnDpSngh <gsingh@inf.ethz.ch>
2019-04-27 04:42:30 +0200

    changed licensing year

--- elina_zonotope/elina_box_assign.c
+++ elina_zonotope/elina_box_assign.c
@@ -1,9 +1,9 @@
 /*
  *
  *  This source file is part of ELINA (ETH LIbrary for Numerical Analysis).
- *  ELINA is Copyright © 2018 Department of Computer Science, ETH Zurich
- *  This software is distributed under GNU Lesser General Public License
- * Version 3.0. For more information, see the ELINA project website at:
+ *  ELINA is Copyright © 2019 Department of Computer Science, ETH Zurich
+ *  This software is distributed under GNU Lesser General Public License Version 3.0.
+ *  For more information, see the ELINA project website at:
  *  http://elina.ethz.ch
  *
  *  THE SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY WARRANTY OF ANY KIND, EITHER
