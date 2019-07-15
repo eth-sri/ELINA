177e02a354cd134b21ea669b266995ca5ec8ee9f
Christoph Müller <muellch@student.ethz.ch>
2019-07-15 03:12:26 +0200

    Simplified the symmetric specialization of interval mul even more

--- fppoly_gpu/fppoly_gpu.cu
+++ fppoly_gpu/fppoly_gpu.cu
@@ -185,23 +185,12 @@ __device__ void elina_double_interval_mul_symmetric_c(float_type *const a_inf,
                                                       const float_type b_inf,
                                                       const float_type b_sup,
                                                       const float_type c) {
-  if (b_inf >= 0) {
-    /*interval b is positive*/
+  if (-b_inf < b_sup) {
     *a_inf = b_sup * -c;
     *a_sup = b_sup * c;
-  } else if (b_sup <= 0) {
-    /* interval b is negative */
+  } else {
     *a_inf = b_inf * c;
     *a_sup = b_inf * -c;
-  } else {
-    /* there is 0 in between for b */
-    if (b_sup < -b_inf) {
-      *a_inf = b_inf * c;
-      *a_sup = b_inf * -c;
-    } else {
-      *a_inf = b_sup * -c;
-      *a_sup = b_sup * c;
-    }
   }
 }
 
