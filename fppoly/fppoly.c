6dd2420993c19d5716a02b067d7931dfe9824050
Gagandeep Singh <gsingh@inf.ethz.ch>
2019-10-14 16:44:08 +0200

    fixed an issue when expression is null

--- fppoly/fppoly.c
+++ fppoly/fppoly.c
@@ -930,7 +930,7 @@ double compute_lb_from_expr(fppoly_internal_t *pr, expr_t *expr, fppoly_t *fp,
   size_t dims = expr->size;
   double res_inf = expr->inf_cst;
   if (expr->inf_coeff == NULL || expr->sup_coeff == NULL) {
-    return 0;
+    return res_inf;
   }
   for (i = 0; i < dims; i++) {
     // if(expr->inf_coeff[i]<0){
@@ -975,7 +975,7 @@ double compute_ub_from_expr(fppoly_internal_t *pr, expr_t *expr, fppoly_t *fp,
   size_t dims = expr->size;
   double res_sup = expr->sup_cst;
   if (expr->inf_coeff == NULL || expr->sup_coeff == NULL) {
-    return 0;
+    return res_sup;
   }
   for (i = 0; i < dims; i++) {
     // if(expr->inf_coeff[i]<0){
