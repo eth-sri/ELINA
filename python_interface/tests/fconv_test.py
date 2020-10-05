bde426e71df115c0082137b30b07f67dc3d9e790
gleb <gleb.makarchuk@gmail.com>
2020-10-05 11:26:12 +0200

    [fconv] Add test for comparing volume of precise relaxation and over-approximation

--- python_interface/tests/fconv_test.py
+++ python_interface/tests/fconv_test.py
@@ -20,21 +20,62 @@
 
 import sys
 sys.path.insert(0, '../')
+import cdd
+import scipy.spatial
 import numpy as np
 from fconv import *
 
-input = np.array([
-    [0.5134633745366125, 1, 1],
-    [0.24054089121901573, 1, 0],
-    [0.25077954363908334, 1, -1],
-    [0.49625233451363315, 0, 1],
-    [0.2163133458481553, 0, -1],
-    [0.7522095824970481, -1, 1],
-    [0.46203194141153164, -1, 0],
-    [0.4550154360636463, -1, -1]
-])
 
-output = fkrelu(input)
+def read_matrix(filename) -> np.ndarray:
+    f = open("../../fconv/octahedron_hrep/" + filename, "r")
+    arr = [list(map(float, l.strip().split())) for l in f.readlines()]
+    return np.array(arr)
 
-assert output.shape[0] > 0
-assert output.shape[1] == (input.shape[1]) * 2 - 1
+
+def hrep2vrep(hrep: np.ndarray, number_type: str, check_bounded=True) -> np.ndarray:
+    """Note that first element of the vertex will be 1."""
+    hrep = cdd.Matrix(hrep, number_type=number_type)
+    hrep.rep_type = cdd.RepType.INEQUALITY
+    vrep = cdd.Polyhedron(hrep).get_generators()
+    vrep = np.array(vrep)
+    if check_bounded and vrep.size > 0:
+        # Making sure that the resulting polyhedron is bounded
+        assert (vrep[:, 0] == 1).all()
+    if vrep.size == 0:
+        return None
+    return vrep
+
+
+def hrep2volume(hrep: np.ndarray, number_type: str) -> float:
+    vrep = hrep2vrep(hrep, number_type)
+    assert (vrep[:, 0] == 1).all()
+    vertices = vrep[:, 1:]
+    return scipy.spatial.ConvexHull(vertices).volume
+
+
+def test_krelu_volume_difference(filename):
+    print("krelu volume test for", filename)
+    hrep = read_matrix(filename)
+
+    res_optimal = krelu_with_cdd(hrep)
+    res_mine = fkrelu(hrep)
+
+    res_concat = np.concatenate([res_optimal, res_mine], axis=0)
+
+    volume_optimal = hrep2volume(res_optimal, "float")
+    volume_mine = hrep2volume(res_mine, "float")
+    volume_concat = hrep2volume(res_concat, "float")
+
+    over_approximation = volume_mine / volume_optimal
+    correctness = volume_concat / volume_optimal
+    # The sound over-approximation is >= 1 and correctness check should = 1.
+    # However, due to numerical errors in computing volume / converting to V
+    # slight deviations are possible - it's okay.
+    print("Over approximation: ", over_approximation)
+    print("Correctness check (should be close to 1)", correctness)
+
+
+# Unfortunately library for computing volume often fails due to numerical errors.
+# Thus it is possible to perform this test only for some of the inputs.
+test_krelu_volume_difference("k3/1.txt")
+test_krelu_volume_difference("k3/5.txt")
