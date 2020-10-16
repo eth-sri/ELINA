55bd19d2aa7231ca83f00b764d672e2f3e782145
Gagandeep Singh <gsingh@student.ethz.ch>
2020-10-17 00:59:41 +0200

    Update zonotope_internal.h

--- elina_zonotope/zonotope_internal.h
+++ elina_zonotope/zonotope_internal.h
@@ -291,7 +291,7 @@ static inline zonotope_noise_symbol_t* zonotope_noise_symbol_add(zonotope_intern
         }
         
         zonotope_aaterm_t* p;
-        if ((expr->c_inf!=INFINITY) && (expr->c_inf==expr->c_sup)){
+        if ((expr->c_inf!=INFINITY) && (-expr->c_inf==expr->c_sup)){
 	    fprintf(stream,"%.20f",expr->c_sup);
         }
         else{
