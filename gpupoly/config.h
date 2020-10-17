f70aa5af9828a16284bd95cd35955aac01be4a06
Francois <francois@gpu-acl.inf.ethz.ch>
2020-10-17 20:54:32 +0200

    Add cmake generated config files

---
+++ gpupoly/config.h
@@ -0,0 +1,34 @@
+/*
+ *  GPUPoly library
+ *  This source file is part of ELINA (ETH LIbrary for Numerical Analysis).
+ *  ELINA is Copyright © 2020 Department of Computer Science, ETH Zurich
+ *  This software is distributed under GNU Lesser General Public License
+ * Version 3.0. For more information, see the ELINA project website at:
+ *  http://elina.ethz.ch
+ *
+ *  THE SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY WARRANTY OF ANY KIND, EITHER
+ *  EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT LIMITED TO ANY WARRANTY
+ *  THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS OR BE ERROR-FREE AND ANY
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
+ *  TITLE, OR NON-INFRINGEMENT.  IN NO EVENT SHALL ETH ZURICH BE LIABLE FOR ANY
+ *  DAMAGES, INCLUDING BUT NOT LIMITED TO DIRECT, INDIRECT,
+ *  SPECIAL OR CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM, OR IN
+ *  ANY WAY CONNECTED WITH THIS SOFTWARE (WHETHER OR NOT BASED UPON WARRANTY,
+ *  CONTRACT, TORT OR OTHERWISE).
+ */
+
+/*!
+  \file src/config.h.in
+  \brief Configuration file.
+  \author Fran&ccedil;ois Serre
+
+   Header containing compilation parameters. This file is configured by CMake
+  prior to compilation.
+ */
+
+#pragma once
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 9
+#define VERSION_PATCH 0
+#define STRONG_FP_SOUNDNESS
