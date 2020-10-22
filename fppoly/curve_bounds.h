20297a555edc5488ecd0050674129dcc15337b82
GgnDpSngh <shanky.22.singh@gmail.com>
2020-10-22 19:50:53 +0200

    improved bounds for sigmoid ad tanh

---
+++ fppoly/curve_bounds.h
@@ -0,0 +1,18 @@
+#ifndef __CURVE_BOUNDS_H_INCLUDED__
+#define __CURVE_BOUNDS_H_INCLUDED__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "fppoly.h"
+
+void compute_S_curve_bounds(double x_lb, double x_ub, bool is_sigmoid,
+                            double* k_lb, double* b_lb, double* k_ub, double* b_ub);
+                            
+                            
+#ifdef __cplusplus
+ }
+#endif
+
+#endif
