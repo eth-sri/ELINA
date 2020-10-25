e1827a79c0adb9fa2e4b660d2ebd7475bbdf4747
gleb <gleb.makarchuk@gmail.com>
2020-10-25 20:26:38 +0100

    [fconv] Clear API for sound bounds

--- fconv/S_curve.cpp
+++ fconv/S_curve.cpp
@@ -3,14 +3,6 @@
 #include <cfenv>
 #include <cassert>
 
-inline void upward() {
-    fesetround(FE_UPWARD);
-}
-
-inline void downward() {
-    fesetround(FE_DOWNWARD);
-}
-
 inline void round(bool up) {
     fesetround(up ? FE_UPWARD : FE_DOWNWARD);
 }
@@ -22,73 +14,81 @@ inline double sigm(double x, bool up) {
     return 1 / denom;
 }
 
-inline double my_tanh(double x, bool up) {
+inline double tanh_my(double x, bool up) {
     round(up);
     return tanh(x);
 }
 
 // y belongs to [0, 1]
-// Returns infimum of the tangent line in the point.
-double sigm_tang_k_inf(double y) {
+double sigm_tang_k(double y, bool sup) {
     assert(0 <= y && y <= 1 && "y should be within range.");
-    downward();
+    round(sup);
     return y * (1 - y);
 }
 
 // y belongs to [-1, 1]
-double tanh_tang_k_inf(double y) {
+double tanh_tang_k(double y, bool sup) {
     assert(-1 <= y && y <= 1 && "y should be within range.");
-    upward();
+    round(!sup);
     double temp = y * y;
-    downward();
+    round(sup);
     return 1 - temp;
 }
 
-double k_of_S_curve_chord_inf(double b_chord, double x_point, double y_point) {
-    round(y_point < b_chord);
-    double numer = y_point - b_chord;
-    downward();
-    double k = numer / x_point;
-    assert(k >= 0 && "k expected to be non-negative.");
-    return k;
+void S_curve_tang_bound(double& k, double& b, double x, bool slope_sup, bool is_sigm) {
+    ASRTF(x != 0, "x should equal zero.");
+    bool up = x > 0;
+
+    double y;
+    if (is_sigm) {
+        y = sigm(x, up);
+        k = sigm_tang_k(y, slope_sup);
+    } else {
+        y = tanh_my(x, up);
+        k = tanh_tang_k(y, slope_sup);
+    }
+    round(!up);
+    double temp = k * x;
+    round(up);
+    b = y - temp;
 }
 
-double b_of_S_curve_tangent(double k_tang, double x_point, double y_point) {
-    assert(k_tang >= 0 && "k of tangent is non-negative.");
-    round(x_point < 0);
-    double temp = k_tang * x_point;
-    round(x_point > 0);
-    return y_point - temp;
+void S_curve_chord_bound(double& k, double& b, double x_lb, double x_ub, bool is_sigm) {
+    ASRTF(x_lb < x_ub, "x_lb < x_ub");
+    ASRTF(x_ub <= 0 || 0 <= x_lb, "x_lb x_ub shouldn't split zero");
+    bool up = x_lb < 0;
+    double y_lb, y_ub;
+    if (is_sigm) {
+        y_lb = sigm(x_lb, up);
+        y_ub = sigm(x_ub, up);
+    } else {
+        y_lb = tanh_my(x_lb, up);
+        y_ub = tanh_my(x_ub, up);
+    }
+    round(!up);
+    double den = x_ub - x_lb;
+    round(up);
+    k = (y_ub - y_lb) / den;
+
+    round(!up);
+    double temp = k * x_lb;
+    round(up);
+    b = y_lb - temp;
 }
 
 void compute_curve_bounds(double x_bound, bool is_sigm,
                           double& k_lb, double& b_lb, double& k_ub, double& b_ub) {
     ASRTF(x_bound != 0, "x_bound cannot be zero.");
 
-    round(true);
-    ASRTF(fegetround() == FE_UPWARD, "making sure fe upward is set correctly.");
-
-    double y_inf, y_sup;
-    if (is_sigm) {
-        y_inf = sigm(x_bound, false);
-        y_sup = sigm(x_bound, true);
-    } else {
-        y_inf = my_tanh(x_bound, false);
-        y_sup = my_tanh(x_bound, true);
-    }
-
-    double b_chord = is_sigm ? 0.5 : 0.0;
+    fesetround(FE_UPWARD);
+    ASRTF(fegetround() == FE_UPWARD, "Making sure that setting floating rounding mode works correctly.");
 
     if (x_bound > 0) {
-        b_lb = b_chord;
-        k_lb = k_of_S_curve_chord_inf(b_chord, x_bound, y_inf);
-        k_ub = is_sigm ? sigm_tang_k_inf(y_sup) : tanh_tang_k_inf(y_sup);
-        b_ub = b_of_S_curve_tangent(k_ub, x_bound, y_sup);
+        S_curve_chord_bound(k_lb, b_lb, 0, x_bound, is_sigm);
+        S_curve_tang_bound(k_ub, b_ub, x_bound, false, is_sigm);
     } else {
-        b_ub = b_chord;
-        k_ub = k_of_S_curve_chord_inf(b_chord, x_bound, y_sup);
-        k_lb = is_sigm ? sigm_tang_k_inf(y_inf) : tanh_tang_k_inf(y_inf);
-        b_lb = b_of_S_curve_tangent(k_lb, x_bound, y_inf);
+        S_curve_chord_bound(k_ub, b_ub, x_bound, 0, is_sigm);
+        S_curve_tang_bound(k_lb, b_lb, x_bound, false, is_sigm);
     }
 
     fesetround(FE_TONEAREST);
