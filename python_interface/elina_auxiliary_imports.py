b347e3c463ea7183eff0e9402c599dd5b56d782d
Mark Mueller <mark.mueller@inf.ethz.ch>
2021-03-18 19:08:43 +0100

    fixed typo

--- python_interface/elina_auxiliary_imports.py
+++ python_interface/elina_auxiliary_imports.py
@@ -25,7 +25,7 @@ import os
 import ctypes
 
 # elina_auxiliary_api = CDLL("libelinaux.so")
-elina_auxiliary_api = ctypes.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)),"../elina_auxillary/libelinaux.so"))
+elina_auxiliary_api = ctypes.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)),"../elina_auxiliary/libelinaux.so"))
 
 
 class CtypesEnum(IntEnum):
